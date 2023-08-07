ecad0361cf1a3020a38df77e3d9d0d4578d8ae8b
nathn00 <qkrdlswo98@gmail.com>
2023-08-07 23:55:31 +0900

    SEMI_FINAL

--- src/components/hooks/api/axiosInterceptor.js
+++ src/components/hooks/api/axiosInterceptor.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
-const rootUrl =
-  process.env.NODE_ENV === "production"
-    ? "http://127.0.0.1:8080"
-    : "http://127.0.0.1:8080";
-
 const useApi = axios.create({
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
-    "Access-Control-Allow-Origin": rootUrl,
+    "Access-Control-Allow-Origin": "http://127.0.0.1:8080",
     "Access-Control-Allow-Credentials": "true",
   },
 });
